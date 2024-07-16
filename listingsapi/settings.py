dfc6186b0caa568e839648552314dc1ddb517325
Derrick Commodore <kryptodamus@gmail.com>
2024-07-16 12:54:40 +0000

    6th

--- listingsapi/settings.py
+++ listingsapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'shops',
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
 
 
 ]
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     
 ]
 
+CORS_ALLOW_ALL_ORIGINS = Truegit 
 ROOT_URLCONF = 'listingsapi.urls'
 
 TEMPLATES = [
