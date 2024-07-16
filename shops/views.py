6ffe2845c4d60419201f0cdb7ea5bce246295280
Derrick Commodore <kryptodamus@gmail.com>
2024-07-16 13:10:47 +0000

    9th

--- shops/views.py
+++ shops/views.py
@@ -29,3 +29,38 @@ class ListingViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(title__icontains=title)
         serializer = ListingSerializer(queryset, many=True)
         return Response(serializer.data)
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Shop, Listing
+from .serializers import ShopSerializer, ListingSerializer
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ShopViewSet(viewsets.ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+    @action(detail=True, methods=['get'])
+    def listings(self, request, pk=None):
+        logger.info("GET /shops/%s/listings", pk)
+        shop = self.get_object()
+        listings = shop.listings.all()
+        serializer = ListingSerializer(listings, many=True)
+        return Response(serializer.data)
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        logger.info("GET /listings/search")
+        queryset = self.get_queryset()
+        title = request.query_params.get('title', None)
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+        serializer = ListingSerializer(queryset, many=True)
+        return Response(serializer.data)
